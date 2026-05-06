b042b797ab0326212f95ff3729cabfa4f9194e5f
김경민 <@DESKTOP-KQSO92H>
2026-05-06 11:38:07 +0900

    0506 hitdirection에서 이름hitMethod로 변경

--- TermProject/src/com/baseball/common/model/SwingData.java
+++ TermProject/src/com/baseball/common/model/SwingData.java
@@ -5,15 +5,15 @@ import java.io.Serializable;
 public class SwingData implements Serializable {
     private static final long serialVersionUID = 1L;
     
-    private int hitDirection; // 1: 밀어치기, 2: 당겨치기
+    private int hitMethod; // 1: 밀어치기, 2: 당겨치기
     private char timing;      // 'f': 빠르게, 's': 느리게
 
     public SwingData(String consoleInput) {
         // "1f" 같은 입력이 들어오면 분리해서 저장합니다.
-        this.hitDirection = Character.getNumericValue(consoleInput.charAt(0));
+        this.hitMethod = Character.getNumericValue(consoleInput.charAt(0));
         this.timing = consoleInput.charAt(1);
     }
 
-    public int getHitDirection() { return hitDirection; }
+    public int getHitDirection() { return hitMethod; }
     public char getTiming() { return timing; }
 }
